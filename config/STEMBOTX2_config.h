610cc6bf1c2e43b57b87abb0af3e8a6db79af8c1
SunilDJPro <tce.sunilprasath@gmail.com>
2024-07-23 18:08:15 +0530

    config_pending

---
+++ config/STEMBOTX2_config.h
@@ -0,0 +1,10 @@
+//Copyright (c) 2024 STEMTEC AI and Robotics Private Limited
+
+
+
+#ifndef STEMBOTX2_CONFIG_H
+#define STEMBOTX2_CONFIG_H
+
+#define LED_PIN 13 //Onboard Teensy LED for debugging
+
+#define STEMBOT_BASE DIFFERENTIAL_DRIVE
